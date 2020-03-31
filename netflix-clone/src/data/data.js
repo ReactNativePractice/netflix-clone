994196b88a6a76c9b93c38ebb916cd069c5764e2
Megan <jeffcoat.megan@gmail.com>
2020-03-31 16:53:07 -0500

    dummb data added in

---
+++ netflix-clone/src/data/data.js
@@ -0,0 +1,43 @@
+const ORANGE_IS_THE_NEW_BLACK = require('../../assets/orange-is-the-new-black.jpeg');
+const STRANGER_THINGS = require('../../assets/stranger-things.jpeg');
+const LUCIFER = require('../../assets/Lucifer.jpg');
+const THE_WITCHER = require('../../assets/The-Witcher.jpg');
+const YOU = require('../../assets/YOU.jpg');
+
+const SHOWS = [
+    {
+        title: 'Orange Is The New Black',
+        summary: 'When a young boy vanishes, a small town uncovers a mystery involving a secret experiment, terrify supernatural forces, and one strange little girl',
+        starring: 'Winona Ryder, David Harbour, Mathew Modine',
+        creator: 'The Duffer Brothers',
+        image: ORANGE_IS_THE_NEW_BLACK,
+      },
+      {
+        title: 'Stranger Things',
+        summary: 'When a young boy vanishes, a small town uncovers a mystery involving a secret experiment, terrify supernatural forces, and one strange little girl',
+        starring: 'Winona Ryder, David Harbour, Mathew Modine',
+        creator: 'The Duffer Brothers',
+        image: STRANGER_THINGS,
+      },
+      {
+          title: 'Lucifer',
+          summary: 'Bored with being the Lord of Hell, the devil relocates to Los Angeles, where he opens a nightclub and forms a connection with a homicide detective.',
+          starring:'Tom Ellis, Lauren German, Kevin Alejandro',
+          creator:'Tom Kapinos',
+          image: LUCIFER,
+      },
+      {
+        title: 'YOU',
+        summary: 'A dangerously charming, intensely obsessive young man goes to extreme measures to insert himself into the lives of those he is transfixed by.',
+        starring: 'Penn Badgley, Elizabeth Lail, Victoria Pedretti',
+        creator: 'Greg Berlanti, Sera Gamble',
+        image: YOU,
+    },
+    {
+        title: 'The Wtcher',
+        summary: 'Geralt of Rivia, a mutated monster-hunter for hire, journeys toward his destiny in a turbulent world where people often prove more wicked than beasts.',
+        starring: 'Henry Cavill, Anya Chalotra, Freya Allan',
+        creator: 'Lauren Schmidt Hissrich',
+        image: THE_WITCHER,
+    },
+]
