2b7d3392daf2670f386fa928b050479e68f03c19
Megan <jeffcoat.megan@gmail.com>
2020-03-31 16:36:42 -0500

    dependancies added and colors added to constants syles.js

---
+++ netflix-clone/src/constants/styles.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+    GREY: {
+        BLACK_RUSSIAN: '#0A0A0B',
+        BRIGHT_GREY: '#515b60',
+    },
+    RED: {
+        FIRE_ENGINE_RED: '#D81120',
+    },
+    WHITE: {
+        WHITE: '#FFFFFF',
+    },
+};
